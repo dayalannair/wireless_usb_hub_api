e24a0268577acf4cb056297632e93527a83e8c77
dayalannair <nairdayalan@gmail.com>
2020-10-18 18:50:12 +0200

    few improvements to thw functionality. Requires Pi pin configuration

--- wireless_usb_hub_api/RaspberryPi_CC2500_interface.c
+++ wireless_usb_hub_api/RaspberryPi_CC2500_interface.c
@@ -1,7 +1,7 @@
 #include "include.h"
 #include <stdio.h>    
 
-extern char paTable[];
+extern char paTable[]; //used to select PA power control settings
 extern char paTableLen;
 
 //define buffers for sending and receiving
@@ -10,51 +10,25 @@ char rxBuffer[4];
 unsigned int i = 0;
 
 void main (void){
-    setup(); //call setup function for Raspberry Pi
-    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT (Watch Dog Timer)
+    TI_CC_SPISetup();                         // Initialize SPI port
     TI_CC_PowerupResetCCxxxx();               // Reset CCxxxx
-    writeRFSettings();                        // Write RF settings to config reg
+    writeRFSettings();                        // Write RF settings to config reg using CC100-CC2500.c file
     TI_CC_SPIWriteBurstReg(TI_CCxxx0_PATABLE, paTable, paTableLen);//Write PATABLE
-// Configure ports -- GDO0 to RX packet info from CCxxxx 
-    portConfig(); 
+    portConfig(); // Configure ports -- GDO0 to RX packet info from CCxxxx 
     interface();
 }
-//sends a string of data 
 
-void setup(){ //sets up SPI for Raspberry Pi (REPLACE WITH GPIO/ CREATE PI HEADER FILE & use RFStudio)
-
-TI_CC_CSn_PxOUT |= TI_CC_CSn_PIN;
-TI_CC_CSn_PxDIR |= TI_CC_CSn_PIN;         // /CS disable
-
-ME1 |= USPIE0;                            // Enable USART0 SPI mode
-UCTL0 = SWRST;                            // Disable USART state machine
-UCTL0 |= CHAR + SYNC + MM;                // 8-bit SPI Master **SWRST**
-UTCTL0 |= CKPH + SSEL1 + SSEL0 + STC;     // SMCLK, 3-pin mode
-UBR00 = 0x02;                             // UCLK/2
-UBR10 = 0x00;                             // 0
-UMCTL0 = 0x00;                            // No modulation
-TI_CC_SPI_USART0_PxSEL |= TI_CC_SPI_USART0_SIMO
-                        | TI_CC_SPI_USART0_SOMI
-                        | TI_CC_SPI_USART0_UCLK;
-                                        // SPI option select
-TI_CC_SPI_USART0_PxDIR |= TI_CC_SPI_USART0_SIMO + TI_CC_SPI_USART0_UCLK;
-                                        // SPI TX out direction
-UCTL0 &= ~SWRST;                          // Initialize USART state machine
-
-
-
-}
 
 void portConfig(){//configure ports for Raspberry Pi (REPLACE WITH GPIO)
 
-    TI_CC_GDO0_PxIES |= TI_CC_GDO0_PIN;       // Int on falling edge (end of pkt)
-    TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;      // Clear flag
-    TI_CC_GDO0_PxIE |= TI_CC_GDO0_PIN;        // Enable int on end of packet
+//----------------------------set up using WiringPi----------------------------------------
+    // TI_CC_GDO0_PxIES |= TI_CC_GDO0_PIN;       // Int on falling edge (end of pkt)
+    // TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;      // Clear flag
+    // TI_CC_GDO0_PxIE |= TI_CC_GDO0_PIN;        // Enable int on end of packet
+//-----------------------------------------------------------------------------------------
 
-    TI_CC_SPIStrobe(TI_CCxxx0_SRX);           // Initialize CCxxxx in RX mode.
-                                                // When a pkt is received, it will
-                                                // signal on GDO0 and wake CPU
-
+    TI_CC_SPIStrobe(TI_CCxxx0_SRX);           // Initialize CCxxxx in RX mode. When a pkt is received, it will signal on GDO0 and wake CPU
     __bis_SR_register(LPM3_bits + GIE);       // Enter LPM3, enable interrupts
 
 
@@ -71,9 +45,10 @@ void send(char[] data){
 //receive function that returns a string 
 char[] receive(){
     char *temp; //holds the transmission's CRC value
-    temp = RFReceivePacket(rxBuffer,3); //stores data in rxBuffer
+    char len = 2 //length of pckt(data and Addr) size byte stripped away in RX function
+    temp = RFReceivePacket(rxBuffer,len); //stores data in rxBuffer
     if (temp == 0x80){
-        return rxBuffer[2];
+        return rxBuffer[1]; //note the address
     }
     else
         error(1);   //Cyclic Redundancy Check failed
