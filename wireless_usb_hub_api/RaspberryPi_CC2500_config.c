97cb88ef786c2765b7a56c1ed0de210555a8103b
DavieByte <69724560+DavieByte@users.noreply.github.com>
2020-11-04 21:04:50 +0200

    Update RaspberryPi_CC2500_config.c
    Configuring SPI between Pi and CC2500. Read and Write methods made here. Registers Initialized

--- wireless_usb_hub_api/RaspberryPi_CC2500_config.c
+++ wireless_usb_hub_api/RaspberryPi_CC2500_config.c
@@ -1,134 +1,4 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2011, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-//------------------------------------------------------------------------------
-//  Description:  This file contains functions that configure the CC1100/2500
-//  device.
-//
-//  Demo Application for MSP430/CC1100-2500 Interface Code Library v1.0
-//
-//  K. Quiring
-//  Texas Instruments, Inc.
-//  July 2006
-//  IAR Embedded Workbench v3.41
-//------------------------------------------------------------------------------
-
-
 #include "include.h"
-#include "RaspberryPi_CC2500_config.h"
-
-#define TI_CC_RF_FREQ  2400                 // 315, 433, 868, 915, 2400
-
-
-
-//------------------------------------------------------------------------------
-//  void writeRFSettings(void)
-//
-//  DESCRIPTION:
-//  Used to configure the CCxxxx registers.  There are five instances of this
-//  function, one for each available carrier frequency.  The instance compiled
-//  is chosen according to the system variable TI_CC_RF_FREQ, assigned within
-//  the header file "TI_CC_hardware_board.h".
-//
-//  ARGUMENTS:
-//      none
-//------------------------------------------------------------------------------
-
-#if TI_CC_RF_FREQ == 2400                          // 2.4GHz
-// Product = CC2500
-// Crystal accuracy = 40 ppm
-// X-tal frequency = 26 MHz
-// RF output power = 0 dBm
-// RX filterbandwidth = 540.000000 kHz
-// Deviation = 0.000000
-// Return state:  Return to RX state upon leaving either TX or RX
-// Datarate = 250.000000 kbps
-// Modulation = (7) MSK
-// Manchester enable = (0) Manchester disabled
-// RF Frequency = 2433.000000 MHz
-// Channel spacing = 199.950000 kHz
-// Channel number = 0
-// Optimization = Sensitivity
-// Sync mode = (3) 30/32 sync word bits detected
-// Format of RX/TX data = (0) Normal mode, use FIFOs for RX and TX
-// CRC operation = (1) CRC calculation in TX and CRC check in RX enabled
-// Forward Error Correction = (0) FEC disabled
-// Length configuration = (1) Variable length packets, packet length configured by the first received byte after sync word.
-// Packetlength = 255
-// Preamble count = (2)  4 bytes
-// Append status = 1
-// Address check = (0) No address check
-// FIFO autoflush = 0
-// Device address = 0
-// GDO0 signal selection = ( 6) Asserts when sync word has been sent / received, and de-asserts at the end of the packet
-// GDO2 signal selection = (11) Serial Clock
-void writeRFSettings(void)
-{
-    // Write register settings
-    TI_CC_SPIWriteReg(TI_CCxxx0_IOCFG2,   0x0B);  // GDO2 output pin config.
-    TI_CC_SPIWriteReg(TI_CCxxx0_IOCFG0,   0x06);  // GDO0 output pin config.
-    TI_CC_SPIWriteReg(TI_CCxxx0_PKTLEN,   0xFF);  // Packet length.
-    TI_CC_SPIWriteReg(TI_CCxxx0_PKTCTRL1, 0x05);  // Packet automation control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_PKTCTRL0, 0x05);  // Packet automation control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_ADDR,     0x01);  // Device address.
-    TI_CC_SPIWriteReg(TI_CCxxx0_CHANNR,   0x00); // Channel number.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCTRL1,  0x07); // Freq synthesizer control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCTRL0,  0x00); // Freq synthesizer control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FREQ2,    0x5D); // Freq control word, high byte
-    TI_CC_SPIWriteReg(TI_CCxxx0_FREQ1,    0x93); // Freq control word, mid byte.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FREQ0,    0xB1); // Freq control word, low byte.
-    TI_CC_SPIWriteReg(TI_CCxxx0_MDMCFG4,  0x2D); // Modem configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_MDMCFG3,  0x3B); // Modem configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_MDMCFG2,  0x73); // Modem configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_MDMCFG1,  0x22); // Modem configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_MDMCFG0,  0xF8); // Modem configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_DEVIATN,  0x00); // Modem dev (when FSK mod en)
-    TI_CC_SPIWriteReg(TI_CCxxx0_MCSM1 ,   0x3F); //MainRadio Cntrl State Machine
-    TI_CC_SPIWriteReg(TI_CCxxx0_MCSM0 ,   0x18); //MainRadio Cntrl State Machine
-    TI_CC_SPIWriteReg(TI_CCxxx0_FOCCFG,   0x1D); // Freq Offset Compens. Config
-    TI_CC_SPIWriteReg(TI_CCxxx0_BSCFG,    0x1C); //  Bit synchronization config.
-    TI_CC_SPIWriteReg(TI_CCxxx0_AGCCTRL2, 0xC7); // AGC control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_AGCCTRL1, 0x00); // AGC control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_AGCCTRL0, 0xB2); // AGC control.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FREND1,   0xB6); // Front end RX configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FREND0,   0x10); // Front end RX configuration.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCAL3,   0xEA); // Frequency synthesizer cal.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCAL2,   0x0A); // Frequency synthesizer cal.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCAL1,   0x00); // Frequency synthesizer cal.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSCAL0,   0x11); // Frequency synthesizer cal.
-    TI_CC_SPIWriteReg(TI_CCxxx0_FSTEST,   0x59); // Frequency synthesizer cal.
-    TI_CC_SPIWriteReg(TI_CCxxx0_TEST2,    0x88); // Various test settings.
-    TI_CC_SPIWriteReg(TI_CCxxx0_TEST1,    0x31); // Various test settings.
-    TI_CC_SPIWriteReg(TI_CCxxx0_TEST0,    0x0B);  // Various test settings.
-}
 
 //Initializing GPIO pins on Pi for SPI
 int setup_gpio(void)
@@ -137,103 +7,112 @@ int setup_gpio(void)
     wiringPiSetup();
     //setting up the SPI interface
     wiringPiSPISetup(SPI_CHAN, SPI_SPEED);
+    //Setting up GPIO pin to identify the state of the CC2500
+    pinMode(GDO2, INPUT);
 
     return 0;
 }
 
-// PATABLE (0 dBm output power)
-extern char paTable[] = {0xFB};
-extern char paTableLen = 1;
-
-#endif
-
-
-//-----------------------------------------------------------------------------
-//  void RFSendPacket(char *txBuffer, char size)
-//
-//  DESCRIPTION:
-//  This function transmits a packet with length up to 63 bytes.  To use this
-//  function, GD00 must be configured to be asserted when sync word is sent and
-//  de-asserted at the end of the packet, which is accomplished by setting the
-//  IOCFG0 register to 0x06, per the CCxxxx datasheet.  GDO0 goes high at
-//  packet start and returns low when complete.  The function polls GDO0 to
-//  ensure packet completion before returning.
-//
-//  ARGUMENTS:
-//      char *txBuffer
-//          Pointer to a buffer containing the data to be transmitted
-//
-//      char size
-//          The size of the txBuffer
-//-----------------------------------------------------------------------------
-void RFSendPacket(char *txBuffer, char size)
+//2 byte array that contains the address of the register and the data to be written to it
+//Then transmits the 1 byte of data via a RF signal
+void write_to_register(uint8_t address, uint8_t value)
 {
-  TI_CC_SPIWriteBurstReg(TI_CCxxx0_TXFIFO, txBuffer, size); // Write TX data
-  TI_CC_SPIStrobe(TI_CCxxx0_STX);           // Change state to TX, initiating
-                                            // data transfer
-
-  while (!(TI_CC_GDO0_PxIN&TI_CC_GDO0_PIN));
-                                            // Wait GDO0 to go hi -> sync TX'ed
-  while (TI_CC_GDO0_PxIN&TI_CC_GDO0_PIN);
-                                            // Wait GDO0 to clear -> end of pkt
-  TI_CC_GDO0_PxIFG &= ~TI_CC_GDO0_PIN;      // After pkt TX, this flag is set.
-                                            // Has to be cleared before existing
+    uint8_t buffer[2] = {address, value};
+    wiringPiSPIDataRW(0, buffer, 2);
 }
 
-
-
-//-----------------------------------------------------------------------------
-//  char RFReceivePacket(char *rxBuffer, char *length)
-//
-//  DESCRIPTION:
-//  Receives a packet of variable length (first byte in the packet must be the
-//  length byte).  The packet length should not exceed the RXFIFO size.  To use
-//  this function, APPEND_STATUS in the PKTCTRL1 register must be enabled.  It
-//  is assumed that the function is called after it is known that a packet has
-//  been received; for example, in response to GDO0 going low when it is
-//  configured to output packet reception status.
-//
-//  The RXBYTES register is first read to ensure there are bytes in the FIFO.
-//  This is done because the GDO signal will go high even if the FIFO is flushed
-//  due to address filtering, CRC filtering, or packet length filtering.
-//
-//  ARGUMENTS:
-//      char *rxBuffer
-//          Pointer to the buffer where the incoming data should be stored
-//      char *length
-//          Pointer to a variable containing the size of the buffer where the
-//          incoming data should be stored. After this function returns, that
-//          variable holds the packet length.
-//
-//  RETURN VALUE:
-//      char
-//          0x80:  CRC OK
-//          0x00:  CRC NOT OK (or no pkt was put in the RXFIFO due to filtering)
-//-----------------------------------------------------------------------------
-char RFReceivePacket(char *rxBuffer, char *length)
+//writes out the contents of an array of desired data to the TX FIFO memory (max of 64 bytes of data)
+//then transmits the entire array via a RF signal 
+void burst_write(uint8_t address, uint8_t *data_array, uint8_t length)
 {
-  char status[2];
-  char pktLen;
-
-  if ((TI_CC_SPIReadStatus(TI_CCxxx0_RXBYTES) & TI_CCxxx0_NUM_RXBYTES))
-  {
-    pktLen = TI_CC_SPIReadReg(TI_CCxxx0_RXFIFO); // Read length byte
-
-    if (pktLen <= *length)                  // If pktLen size <= rxBuffer
+    uint8_t buffer[length + 1];
+    buffer[0] = address | (uint8_t)TI_CCxxx0_WRITE_BURST;
+    for(uint8_t i = 0; i < length; i++)
     {
-      TI_CC_SPIReadBurstReg(TI_CCxxx0_RXFIFO, rxBuffer, pktLen); // Pull data
-      *length = pktLen;                     // Return the actual size
-      TI_CC_SPIReadBurstReg(TI_CCxxx0_RXFIFO, status, 2);
-                                            // Read appended status bytes
-      return (char)(status[TI_CCxxx0_LQI_RX]&TI_CCxxx0_CRC_OK);
-    }                                       // Return CRC_OK bit
-    else
-    {
-      *length = pktLen;                     // Return the large size
-      TI_CC_SPIStrobe(TI_CCxxx0_SFRX);      // Flush RXFIFO
-      return 0;                             // Error
+        buffer[i + 1] = data_array[i];
     }
-  }
-  else
-      return 0;                             // Error
+    wiringPiSPIDataRW(0, buffer, length + 1);
+}
+
+//2 byte array, first locations contains the address of the data we want to read from 
+//the second array location remains empty to have the data is loaded into it during the SPI read process
+uint8_t read_from_register(uint8_t address)
+{
+    uint8_t buffer[2] = {address | TI_CCxxx0_READ_SINGLE, 0};
+    wiringPiSPIDataRW(0, buffer, 2);
+    return buffer[1];
+}
+
+//An empty array is loaded into the function and the desired address
+//then the contents of the chosen address loads into the array
+void burst_read(uint8_t address, uint8_t *data_array, uint8_t length)
+{
+    uint8_t buffer[length + 1];
+    buffer[0] = address | (uint8_t)TI_CCxxx0_READ_BURST;
+    wiringPiSPIDataRW(0, buffer, length + 1);
+    for(uint8_t i = 0; i < length; i++)
+    {
+        data_array[i] = buffer[i + 1];
+    }
+}
+
+//This allows access to the multiple features that the CC2500 is capable of
+void command_strobe(uint8_t address)
+{
+    uint8_t buffer[1] = {address};
+    wiringPiSPIDataRW(0, buffer, 1);
+}
+
+//This loads in the desired values of all the registers of the CC2500
+//Things such as RF frequency can be configured here
+void initializeCC2500()
+{
+    write_to_register(TI_CCxxx0_IOCFG2, 0x06);
+    write_to_register(TI_CCxxx0_IOCFG1, 0x2E);
+    write_to_register(TI_CCxxx0_IOCFG0, 0x06);
+    write_to_register(TI_CCxxx0_FIFOTHR, 0x07);
+    write_to_register(TI_CCxxx0_SYNC1, 0xD3);
+    write_to_register(TI_CCxxx0_SYNC0, 0x91);
+    write_to_register(TI_CCxxx0_PKTLEN, 0xFF);
+    write_to_register(TI_CCxxx0_PKTCTRL1, 0x04);
+    write_to_register(TI_CCxxx0_PKTCTRL0, 0x05);
+    write_to_register(TI_CCxxx0_ADDR, 0x00);
+    write_to_register(TI_CCxxx0_CHANNR, 0x00);
+    write_to_register(TI_CCxxx0_FSCTRL1, 0x08);
+    write_to_register(TI_CCxxx0_FSCTRL0, 0x00);
+    //FREQ2 - FREQ0 configures the output RF frequency
+    write_to_register(TI_CCxxx0_FREQ2, 0x25);
+    write_to_register(TI_CCxxx0_FREQ1, 0x80);
+    write_to_register(TI_CCxxx0_FREQ0, 0x00); // hopefully 900Mhz frequency
+    write_to_register(TI_CCxxx0_MDMCFG4, 0x86);
+    write_to_register(TI_CCxxx0_MDMCFG3, 0x83);
+    write_to_register(TI_CCxxx0_MDMCFG2, 0x03);
+    write_to_register(TI_CCxxx0_MDMCFG1, 0x22);
+    write_to_register(TI_CCxxx0_MDMCFG0, 0xF8);
+    write_to_register(TI_CCxxx0_DEVIATN, 0x44);
+    write_to_register(TI_CCxxx0_MCSM2, 0x07);
+    write_to_register(TI_CCxxx0_MCSM1, 0x30);
+    write_to_register(TI_CCxxx0_MCSM0, 0x18);
+    write_to_register(TI_CCxxx0_FOCCFG, 0x16);
+    write_to_register(TI_CCxxx0_BSCFG, 0x6C);
+    write_to_register(TI_CCxxx0_AGCCTRL2, 0x03);
+    write_to_register(TI_CCxxx0_AGCCTRL1, 0x40);
+    write_to_register(TI_CCxxx0_AGCCTRL0, 0x91);
+    write_to_register(TI_CCxxx0_WOREVT1, 0x87);
+    write_to_register(TI_CCxxx0_WOREVT0, 0x6B);
+    write_to_register(TI_CCxxx0_WORCTRL, 0xF8);
+    write_to_register(TI_CCxxx0_FREND1, 0x56);
+    write_to_register(TI_CCxxx0_FREND0, 0x10);
+    write_to_register(TI_CCxxx0_FSCAL3, 0xA9);
+    write_to_register(TI_CCxxx0_FSCAL2, 0x0A);
+    write_to_register(TI_CCxxx0_FSCAL1, 0x00);
+    write_to_register(TI_CCxxx0_FSCAL0, 0x11);
+    write_to_register(TI_CCxxx0_RCCTRL1, 0x41);
+    write_to_register(TI_CCxxx0_RCCTRL0, 0x00);
+    write_to_register(TI_CCxxx0_FSTEST, 0x59);
+    write_to_register(TI_CCxxx0_PTEST, 0x7F);
+    write_to_register(TI_CCxxx0_AGCTEST, 0x3F);
+    write_to_register(TI_CCxxx0_TEST2, 0x88);
+    write_to_register(TI_CCxxx0_TEST1, 0x31);
+    write_to_register(TI_CCxxx0_TEST0, 0x0B);
 }
