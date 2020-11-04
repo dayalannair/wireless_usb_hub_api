331275d3ca6dbae89ea7dcfb8d43467c8f964269
DavieByte <69724560+DavieByte@users.noreply.github.com>
2020-11-04 21:03:15 +0200

    Update __init__.py
    Cleaning up repo

--- wireless_usb_hub_api/__init__.py
+++ wireless_usb_hub_api/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Wireless USB Hub API."""
 
-__author__ = """Dayalan Nair"""
+__author__ = """Dayalan Nair & David Moore"""
 __email__ = 'nairdayalan@gmail.com'
 __version__ = '0.1.0'
